1736f358f5798f6bbf162ea9a565f202f42c6c49
Maryamk16 <maryamk@mindfiresolutions.com>
2017-03-08 16:12:29 +0530

    Add files via upload

---
+++ testing.java
@@ -0,0 +1,33 @@
+package MyPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class testing
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Maryam Khan\\workspace\\Selenium Practice\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://www.google.com");
+		
+		//String CurrentUrl = driver.getCurrentUrl();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.quit();
+		
+		
+
+	}
+
+}
